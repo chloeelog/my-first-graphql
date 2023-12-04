344ee7f77c6061cee7813f266cf7f019370781de
Chloé <chloeelog@gmail.com>
2023-12-04 22:50:06 +0900

    feat: set search page base

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { RelayEnvironmentProvider } from "react-relay/hooks";
 import { RelayEnvironment } from "./RelayEnvironment";
+
+import { SearchPage } from "./SearchPage";
 
 function App() {
   return (
     <RelayEnvironmentProvider environment={RelayEnvironment}>
-      <div />
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<SearchPage />} />
+        </Routes>
+      </BrowserRouter>
     </RelayEnvironmentProvider>
   );
 }
