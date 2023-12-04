cbc86bd4fbead990f8a329b256bf802e450b67e7
Chloé <chloeelog@gmail.com>
2023-12-04 23:58:07 +0900

    feat: handle pagination

--- src/components/ListSection/ListSection.tsx
+++ src/components/ListSection/ListSection.tsx
@@ -4,51 +4,27 @@ import { useSearchParams } from "react-router-dom";
 import { graphql } from "relay-runtime";
 
 import { ListSectionRepositorySearchQuery } from "./__generated__/ListSectionRepositorySearchQuery.graphql";
-import { RepositoryItem } from "./RepositoryItem";
 
-const RepositorySearchQuery = graphql`
-  query ListSectionRepositorySearchQuery($keyword: String!) {
-    search(query: $keyword, type: REPOSITORY, first: 10) {
-      repositoryCount
-      edges {
-        node {
-          ...RepositoryItem_repository
-          ... on Repository {
-            id
-          }
-        }
-      }
-    }
-  }
-`;
+import { RepositoryPagination } from "./RepositoryPagination";
 
 export const ListSection = () => {
   const [searchParams] = useSearchParams();
   const keyword = searchParams.get("keyword") || "";
 
   const data = useLazyLoadQuery<ListSectionRepositorySearchQuery>(
-    RepositorySearchQuery,
+    graphql`
+      query ListSectionRepositorySearchQuery($keyword: String!) {
+        ...RepositoryPagination @arguments(keyword: $keyword)
+      }
+    `,
     {
       keyword,
     }
   );
 
-  if (!data.search.repositoryCount || !data.search.edges?.length) {
-    return <p>검색 결과가 없어요.</p>;
-  }
   return (
     <ul>
-      {data.search.edges.map((repository) => {
-        if (!repository || !repository.node) {
-          return null;
-        }
-        return (
-          <RepositoryItem
-            key={repository.node.id}
-            repository={repository.node}
-          />
-        );
-      })}
+      <RepositoryPagination data={data} />
     </ul>
   );
 };
